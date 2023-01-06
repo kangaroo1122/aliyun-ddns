c9da5b59681886818d629fde2e2fd885570828b8
kangaroo1122 <kangaroohy1122@gmail.com>
2023-01-06 13:43:41 +0800

    代码优化

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -1,22 +1,4 @@
 axios.interceptors.response.use(resp => {
-    if (resp.request.responseType === 'blob') {
-        let blob = new Blob([resp.data])
-        let contentDisposition = resp.headers['content-disposition'];
-        let fileName = decodeURI(contentDisposition.replace("attachment;filename=", ""));
-        if ('download' in document.createElement('a')) { //支持a标签download的浏览器
-            const link = document.createElement('a')//创建a标签
-            link.download = fileName//a标签添加属性
-            link.style.display = 'none'
-            link.href = URL.createObjectURL(blob)
-            document.body.appendChild(link)
-            link.click()//执行下载
-            URL.revokeObjectURL(link.href) //释放url
-            document.body.removeChild(link)//释放标签
-        } else { //其他浏览器
-            navigator.msSaveBlob(blob, fileName)
-        }
-        return;
-    }
     return resp;
 }, resp => {
     console.log(resp)
